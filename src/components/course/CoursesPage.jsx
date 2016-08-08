18dc60210cfba8d5960fdb5676de343249e4fd42
Kunuk Nykjær <kunukn@gmail.com>
2016-08-08 14:19:45 +0200

    add courses page

---
+++ src/components/course/CoursesPage.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Courses</h1>
+            </div>
+        );
+    } 
+}
+
+export default CoursesPage;
