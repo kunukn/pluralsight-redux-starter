81bddce1440f2a52dee1a243f23c983000a83f36
Kunuk Nykjær <kunukn@gmail.com>
2016-08-17 14:07:50 +0200

    Dispatch Action on Page Load

--- src/components/course/CoursesPage.jsx
+++ src/components/course/CoursesPage.jsx
@@ -7,24 +7,26 @@ import * as coursesActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
+/*
     this.state = {
       course: {title: ''}
-    };
+    };*/
 
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
+/*    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);*/
   }
 
-  onTitleChange(event){
+/*  onTitleChange(event){
     const course = this.state.course;
     course.title = event.target.value;
     this.setState({course: course});
-  }
+  }*/
 
+/*
   onClickSave(event){
    this.props.actions.createCourse(this.state.course);
   }
+*/
 
   courseRow(course, index){
     return (
@@ -38,14 +40,14 @@ class CoursesPage extends React.Component {
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
 
-        <h2>Add Course</h2>
+  {/*      <h2>Add Course</h2>
         <input type="text"
                onChange={this.onTitleChange}
                value={this.state.course.title}/>
 
         <input type="submit"
                value="Save"
-               onClick={this.onClickSave}/>
+               onClick={this.onClickSave}/>*/}
       </div>
     );
   }
